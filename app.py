3603ff6d5a92f25e6e8b53bec2a4af24e623d3ff
sksarvesh007 <bt22csd002@iiitn.ac.in>
2024-10-19 16:52:42 +0530

    pipelined into main.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from src.audio_scraper import audio_extractor
 from src.splitter import split_file
+from src.transcript import transcribe_audio
 import os 
-youtube_link = "https://www.youtube.com/watch?v=Tt4_enX63K0"
+youtube_link = "https://www.youtube.com/watch?v=2QRlvKSzyVw&list=PLD80i8An1OEGqqXeNZ5w0IBmeZcxpZEYL"
 audio_extractor(youtube_link)
 audio_file = os.listdir('audio')[0]
 file_dir = "audio/" + audio_file
 split_file(file_dir)
+transcribe_audio()
+print("Transcription complete")
--- src/transcript.py
+++ src/transcript.py
@@ -2,30 +2,26 @@ import os
 from groq import Groq
 import dotenv
 
-dotenv.load_dotenv()
-groq_api_key = os.getenv("GROQ_API_KEY")
-os.environ["GROQ_API_KEY"] = groq_api_key
-
-client = Groq()
-
-cwd = os.getcwd()
-print(cwd)
-audio_dir = os.path.join(cwd, 'splitted')  
-filename = os.path.join(audio_dir, 'part1.webm')  
-script_dir = cwd
-
-with open(filename, "rb") as file:
-    transcription = client.audio.transcriptions.create(
-        file=(filename, file.read()),
-        model="whisper-large-v3-turbo",
-        prompt="make me a transcript of the audio file",
-        language="ja" , 
-        response_format="verbose_json",
-    )
-    transcript_path = os.path.join(script_dir, 'transcript.txt')
-    text_path = os.path.join(script_dir, 'text.txt')
-    with open(transcript_path, 'w', encoding='utf-8') as f:
-        for item in transcription.segments:
-            f.write("%s\n" % item)
-    with open(text_path, 'w', encoding='utf-8') as f :
-        f.write(transcription.text)
+def transcribe_audio(audio_dir='splitted', filename='part1.webm', language='ja'):
+    dotenv.load_dotenv()
+    groq_api_key = os.getenv("GROQ_API_KEY")
+    os.environ["GROQ_API_KEY"] = groq_api_key
+    client = Groq()
+    cwd = os.getcwd()
+    audio_path = os.path.join(cwd, audio_dir, filename)
+    script_dir = cwd
+    with open(audio_path, "rb") as file:
+        transcription = client.audio.transcriptions.create(
+            file=(filename, file.read()),
+            model="whisper-large-v3-turbo",
+            prompt="make me a transcript of the audio file",
+            language=language,
+            response_format="verbose_json",
+        )
+        transcript_path = os.path.join(script_dir, 'transcript.txt')
+        text_path = os.path.join(script_dir, 'text.txt')
+        with open(transcript_path, 'w', encoding='utf-8') as f:
+            for item in transcription.segments:
+                f.write("%s\n" % item)
+        with open(text_path, 'w', encoding='utf-8') as f:
+            f.write(transcription.text)
