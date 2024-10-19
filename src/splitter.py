98c752f4809ea0d4e0c4b31233c944ea367b6790
sksarvesh007 <bt22csd002@iiitn.ac.in>
2024-10-19 10:10:42 +0530

    changes

--- src/splitter.py
+++ src/splitter.py
@@ -1,9 +1,6 @@
-#i have an audio file ,print the size of the file 
 import os
 
 file_name = "Advanced Algorithms (COMPSCI 224), Lecture 1 [0JUN9aDxVmI].webm"
-
-#i want you to check if the file size is more than 24MB , then make a folder and split into size of 24MB each and name part 1 , part 2 etc
 if os.path.getsize(file_name) > 24*1024*1024:
     os.mkdir("splitted")
     with open(file_name, 'rb') as f:
--- src/transcript.py
+++ src/transcript.py
@@ -1,7 +1,9 @@
 import os
 from groq import Groq
-
-os.environ["GROQ_API_KEY"] = "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"
+import dotenv 
+dotenv.load_dotenv()
+groq_api_key = os.getenv("GROQ_API_KEY")
+os.environ["GROQ_API_KEY"] = groq_api_key
 client = Groq()
 filename = os.path.dirname(__file__) + "\splitted\part1.webm" 
 with open(filename, "rb") as file:
@@ -11,8 +13,6 @@ with open(filename, "rb") as file:
     prompt="make me a transcript of the audio file ",
     response_format="verbose_json",
     )
-    print(transcription.segments)
-    #save in a txt file 
     with open('transcript.txt', 'w') as f:
         for item in transcription.segments:
             f.write("%s\n" % item)
